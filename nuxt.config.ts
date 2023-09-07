83fa934095d6837781f1c584be34386d0159d70a
d3vkk <donaldkk22@yahoo.com>
2023-09-07 19:04:15 +0300

    refactor(seo): Add robots.txt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // modules: ["@nuxt/postcss8"],
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@nuxtjs/robots"],
   css: ["@/assets/css/index.css"],
   components: ["~/views/landing", "~/views", "~/components"],
   postcss: {
@@ -9,5 +9,9 @@ export default defineNuxtConfig({
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  robots: {
+    UserAgent: "*",
+    Disallow: "",
   },
 });
