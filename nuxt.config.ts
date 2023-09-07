1a62dec09f3609315f6592f736b8a32579714a99
d3vkk <donaldkk22@yahoo.com>
2023-09-07 14:54:40 +0300

    refactor: Add pinia state management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // modules: ["@nuxt/postcss8"],
+  modules: ["@pinia/nuxt"],
   css: ["@/assets/css/index.css"],
   components: ["~/views/landing", "~/views", "~/components"],
   postcss: {
