3fb5cee9b7829553adc5e24e3ffbbc5f09ead3d6
d3vkk <donaldkk22@yahoo.com>
2024-09-02 13:17:22 +0300

    chore: Update robots.txt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // modules: ["@nuxt/postcss8"],
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
   modules: ["@pinia/nuxt", "@nuxtjs/robots"],
   css: ["@/assets/css/index.css"],
   components: ["~/views/landing", "~/views", "~/components"],
@@ -9,9 +10,5 @@ export default defineNuxtConfig({
       tailwindcss: {},
       autoprefixer: {},
     },
-  },
-  robots: {
-    UserAgent: "*",
-    Disallow: "",
   },
 });
